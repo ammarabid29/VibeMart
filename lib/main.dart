b3671dba67a5d9b7024a6691b33de10ef9974dbf
ammarabid29 <ammar.abid@algorepublic.com>
2025-04-10 12:10:30 +0500

    App name and logo updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const VibeMart());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class VibeMart extends StatelessWidget {
+  const VibeMart({super.key});
 
   // This widget is the root of your application.
   @override
